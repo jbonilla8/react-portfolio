4800e714c8dc87e8e2a9d97a1ba141686da680a3
jbonilla8 <jalisabonilla10@gmail.com>
2020-09-24 15:36:00 -0400

    GA stuff

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Fragment } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Router } from 'react-router-dom';
 import ProjectPage from './components/ProjectPage';
 import HomePage from './components/HomePage';
 import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
 
 function App() {
+
   ReactGA.initialize('UA-170020240-1');
-  ReactGA.pageview(window.location.pathname + window.location.search);
-  
+
+  var history = createBrowserHistory();
+
+  history.listen((location) => {
+    window.ga('set', 'page', location.pathname + location.search);
+    window.ga('send', 'pageview');
+  });
+
   return (
     <Fragment>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/project/:id" component={ProjectPage} />
+      <Router history={history}>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/project/:id" component={ProjectPage} />
+      </Router>
     </Fragment>
   );
 }
