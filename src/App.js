0b5b93d60e9a11ef526171ecae81b91263e21e73
jbonilla8 <jalisabonilla10@gmail.com>
2020-09-24 15:30:53 -0400

    added google analytics

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { Fragment } from 'react';
 import { Route } from 'react-router-dom';
 import ProjectPage from './components/ProjectPage';
 import HomePage from './components/HomePage';
+import ReactGA from 'react-ga';
 
 function App() {
+  ReactGA.initialize('UA-170020240-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
   
   return (
     <Fragment>
